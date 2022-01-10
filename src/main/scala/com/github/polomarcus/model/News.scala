b1ff38bc586f3cc295b7ed155a9a441323cae3c1
Paul Leclercq <paleclercq@gmail.com>
2022-01-10 19:07:39 +0100

    model: add news, authors

--- src/main/scala/com/github/polomarcus/model/News.scala
+++ src/main/scala/com/github/polomarcus/model/News.scala
@@ -3,4 +3,13 @@ package com.github.polomarcus.model
 import java.util.Date
 
 
-case class News (title: String, description: String, date: Date, order: Integer, presenter: String)
+case class News (title: String,
+                 description: String,
+                 date: Date,
+                 order: Integer,
+                 presenter: String,
+                 authors: List[String],
+                 redactor: String,
+                // redactorDeputy: List[String],
+                // publisher: String,
+                 url: String)
