02df6f3704061e20884ffb78ece1ea87ca1101c5
Paul Leclercq <paleclercq@gmail.com>
2022-01-18 15:18:37 +0100

    cleaning

--- init-stack-with-data.sh
+++ init-stack-with-data.sh
@@ -9,3 +9,5 @@ docker-compose -f src/test/docker/docker-compose.yml up -d
 sleep 3
 
 # Insert data from 2013 to 2021 to PG
+echo 'Insert data from 2013 to 2021 to PG'
+sbt "runMain com.github.polomarcus.main.SaveTVNewsToPostgres"
