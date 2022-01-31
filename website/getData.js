e202519eefd670cfeabe4f95301e1a7e5f4b1737
Paul Leclercq <paleclercq@gmail.com>
2022-01-31 18:54:17 +0100

    feat(website): new graph

--- website/getData.js
+++ website/getData.js
@@ -9,12 +9,22 @@ fetch( "https://polomarcus.github.io/television-news-analyser/website/data-aggre
     //create Tabulator on DOM element with id "example-table"
     var table = new Tabulator("#example-table", {
        // height:205, // set height of table (in CSS or here), this enables the Virtual DOM and improves render speed dramatically (can be any valid css height value)
-        data:latestNews, //assign data to table
-        layout:"fitData",
+        data: latestNews, //assign data to table
+        layout: "fitDataFill",
         columns:[ //Define Table Columns
             {title:"Date", field:"date", sorter:"date"},
             {title:"Media", field:"media", formatter:"plaintext"},
             {title:"Titre", field:"url",  formatter:"link",formatterParams:{labelField:"title",target:"_blank"}},
         ],
     });
+
+    var setupFilter = function(fieldName) {
+                var filterEl = document.getElementById(fieldName + "-filter-value");
+                // Trigger setFilter function with correct parameters
+                function updateFilter(){
+                        table.setFilter(fieldName,'like', filterEl.value.toLowerCase());
+                }
+                filterEl.addEventListener("keyup", updateFilter);
+            };
+        setupFilter("title");
 });
