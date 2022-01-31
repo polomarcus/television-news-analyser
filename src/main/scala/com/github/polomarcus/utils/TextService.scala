97b0c95b0b2373dbe78e54040ae5d4bcd64955a7
Paul Leclercq <paleclercq@gmail.com>
2022-01-31 10:42:53 +0100

    fix: detect with 3 words global warming (#7)

--- src/main/scala/com/github/polomarcus/utils/TextService.scala
+++ src/main/scala/com/github/polomarcus/utils/TextService.scala
@@ -2,10 +2,12 @@ package com.github.polomarcus.utils
 
 import com.typesafe.scalalogging.Logger
 
+import scala.util.matching.Regex
+
 object TextService {
     val logger = Logger(TextService.getClass)
-
+    val pattern = new Regex("(réchauffement|dérèglement|changement)(|s) climatique(|s)")
     def containsWordGlobalWarming(description: String) : Boolean = {
-      description.toLowerCase().contains("climatique")
+      pattern.findFirstIn(description.toLowerCase).isDefined
     }
 }
--- src/test/scala/TextServiceTest.scala
+++ src/test/scala/TextServiceTest.scala
@@ -2,9 +2,31 @@ import com.github.polomarcus.utils.TextService
 import org.scalatest.funsuite.AnyFunSuite
 
 class TextServiceTest extends AnyFunSuite {
-  test("containsWordGlobalWarming") {
-    assert(TextService.containsWordGlobalWarming("pizza") == false)
-    assert(TextService.containsWordGlobalWarming("réchauffement climatique") == true)
-    assert(TextService.containsWordGlobalWarming("changement climatique") == true)
-  }
+    test("containsWordGlobalWarming") {
+      assert(!TextService.containsWordGlobalWarming("pizza"))
+    }
+
+    test("containsWordGlobalWarming - aléas climatiques") {
+      assert(!TextService.containsWordGlobalWarming("aléas climatiques"))
+    }
+
+    test("réchauffement") {
+      assert(TextService.containsWordGlobalWarming("réchauffement climatique"))
+    }
+
+    test("dérèglement") {
+      assert(TextService.containsWordGlobalWarming("dérèglement climatique"))
+    }
+
+    test("changement") {
+      assert(TextService.containsWordGlobalWarming("changement climatique"))
+    }
+
+    test("changements") {
+      assert(TextService.containsWordGlobalWarming("changements climatiques"))
+    }
+
+    test("changements with capital letters") {
+      assert(TextService.containsWordGlobalWarming("Les Changements Climatiques"))
+    }
 }
