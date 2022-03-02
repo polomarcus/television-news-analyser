95658707a13dc3d0a6208fefb1bf8e00baa80d73
Paul Leclercq <paleclercq@gmail.com>
2022-03-02 15:38:16 +0100

    feat: improve global warming detection

--- src/main/scala/com/github/polomarcus/utils/TextService.scala
+++ src/main/scala/com/github/polomarcus/utils/TextService.scala
@@ -5,9 +5,12 @@ import com.typesafe.scalalogging.Logger
 import scala.util.matching.Regex
 
 object TextService {
-    val logger = Logger(TextService.getClass)
-    val pattern = new Regex("(réchauffement|dérèglement|changement)(|s) climatique(|s)")
-    def containsWordGlobalWarming(description: String) : Boolean = {
-      pattern.findFirstIn(description.toLowerCase).isDefined
-    }
+  val logger = Logger(TextService.getClass)
+  val pattern = new Regex("(réchauffement|dérèglement|changement)(|s) climatique(|s)")
+  val pattern2 = new Regex("(réchauffement|dérèglement|changement) du climat")
+  def containsWordGlobalWarming(description: String): Boolean = {
+    pattern.findFirstIn(description.toLowerCase).isDefined || pattern2
+      .findFirstIn(description.toLowerCase)
+      .isDefined
+  }
 }
--- src/test/scala/TextServiceTest.scala
+++ src/test/scala/TextServiceTest.scala
@@ -2,31 +2,37 @@ import com.github.polomarcus.utils.TextService
 import org.scalatest.funsuite.AnyFunSuite
 
 class TextServiceTest extends AnyFunSuite {
-    test("containsWordGlobalWarming") {
-      assert(!TextService.containsWordGlobalWarming("pizza"))
-    }
+  test("containsWordGlobalWarming") {
+    assert(!TextService.containsWordGlobalWarming("pizza"))
+  }
 
-    test("containsWordGlobalWarming - aléas climatiques") {
-      assert(!TextService.containsWordGlobalWarming("aléas climatiques"))
-    }
+  test("containsWordGlobalWarming - aléas climatiques") {
+    assert(!TextService.containsWordGlobalWarming("aléas climatiques"))
+  }
 
-    test("réchauffement") {
-      assert(TextService.containsWordGlobalWarming("réchauffement climatique"))
-    }
+  test("réchauffement") {
+    assert(TextService.containsWordGlobalWarming("réchauffement climatique"))
+  }
 
-    test("dérèglement") {
-      assert(TextService.containsWordGlobalWarming("dérèglement climatique"))
-    }
+  test("dérèglement") {
+    assert(TextService.containsWordGlobalWarming("dérèglement climatique"))
+  }
 
-    test("changement") {
-      assert(TextService.containsWordGlobalWarming("changement climatique"))
-    }
+  test("changement") {
+    assert(TextService.containsWordGlobalWarming("changement climatique"))
+  }
 
-    test("changements") {
-      assert(TextService.containsWordGlobalWarming("changements climatiques"))
-    }
+  test("changements") {
+    assert(TextService.containsWordGlobalWarming("changements climatiques"))
+  }
 
-    test("changements with capital letters") {
-      assert(TextService.containsWordGlobalWarming("Les Changements Climatiques"))
-    }
+  test("changements with capital letters") {
+    assert(TextService.containsWordGlobalWarming("Les Changements Climatiques"))
+  }
+
+  test("with 'du climat'") {
+    assert(TextService.containsWordGlobalWarming("dérèglement du climat"))
+    assert(TextService.containsWordGlobalWarming("réchauffement du climat"))
+    assert(TextService.containsWordGlobalWarming("changement du climat"))
+  }
 }
