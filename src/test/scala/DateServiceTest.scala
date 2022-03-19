e8bfef03784c02bd416fd2706ad6941cae12c482
Paul Leclercq <paleclercq@gmail.com>
2022-03-19 11:06:35 +0100

    feat(fr2): add new subtitle (#22)

--- src/test/scala/DateServiceTest.scala
+++ src/test/scala/DateServiceTest.scala
@@ -1,18 +1,28 @@
 import com.github.polomarcus.utils.DateService
+import com.github.polomarcus.utils.DateService.timezone
 import org.scalatest.funsuite.AnyFunSuite
 
 import java.sql.Timestamp
+import java.text.SimpleDateFormat
 import java.time.{LocalDate, ZoneId}
-import java.util.Date
+import java.util.{Date, TimeZone}
 
 class DateServiceTest extends AnyFunSuite {
+  val timezone = TimeZone.getTimeZone("UTC+2")
+  val format = new SimpleDateFormat("dd/MM/yyyy")
+  format.setTimeZone(timezone)
+
   test("getTimestampFrance2") {
-    assert(DateService.getTimestampFrance2  ("Diffusé le 08/01/2022") == new Timestamp(new Date("01/08/2022").getTime))
+    assert(
+      DateService.getTimestampFrance2("Diffusé le 08/01/2022") ==
+        new Timestamp(format.parse("08/01/2022").getTime))
   }
 
   test("getTimestampTF1") {
-    assert(DateService.getTimestampTF1  ("Publié le 10 décembre 2020 à 20h08") == new Timestamp(new Date("12/10/2020").getTime))
-    assert(DateService.getTimestampTF1  ("Publié hier à 20h39").getTime >= 0)
-    assert(DateService.getTimestampTF1  ("Publié aujourd'hui à 20h39").getTime >= 0)
+    assert(
+      DateService.getTimestampTF1("Publié le 10 décembre 2020 à 20h08") ==
+        new Timestamp(format.parse("10/12/2020").getTime))
+    assert(DateService.getTimestampTF1("Publié hier à 20h39").getTime >= 0)
+    assert(DateService.getTimestampTF1("Publié aujourd'hui à 20h39").getTime >= 0)
   }
 }
