b14f33a9e87defb2862580072fd327f326d5cb32
Paul Leclercq <paleclercq@gmail.com>
2022-05-02 22:39:08 +0200

    feat: add 13h tv news show

--- src/main/scala/com/github/polomarcus/html/Getter.scala
+++ src/main/scala/com/github/polomarcus/html/Getter.scala
@@ -9,9 +9,14 @@ object Getter {
   implicit val ec = FutureService.ec
 
   val france2UrlPagination = "https://www.francetvinfo.fr/replay-jt/france-2/20-heures"
+  val france213hUrlPagination = "https://www.francetvinfo.fr/replay-jt/france-2/13-heures"
+
   val france3UrlPagination = "https://www.francetvinfo.fr/replay-jt/france-3/19-20"
+  val france313hUrlPagination = "https://www.francetvinfo.fr/replay-jt/france-3/12-13"
+
   val tf1UrlPagination = "https://www.tf1info.fr/emission/le-20h-11001/extraits"
   val tf1WeekendUrlPagination = "https://www.tf1info.fr/emission/le-we-12559/extraits"
+  val tf113hPagination = "https://www.tf1info.fr/emission/le-13h-10927/extraits"
 
   /**
    * 2 choices to parse :
@@ -22,10 +27,10 @@ object Getter {
    * @param media: tf1 or france3 or france2 (default)
    */
   def getNews(start: Integer, end: Integer, media: String): List[News] = {
-    val urlMedia = media match {
-      case "tf1" => tf1UrlPagination
-      case "france3" => france3UrlPagination
-      case _ => france2UrlPagination
+    val (urlMedia, urlMedia13h) = media match {
+      case "tf1" => (tf1UrlPagination, tf113hPagination)
+      case "france3" => (france3UrlPagination, france313hUrlPagination)
+      case _ => (france2UrlPagination, france213hUrlPagination)
     }
     val newsList = (start.toLong to end.toLong by 1).map { page =>
       val pagination = getPagination(page)
@@ -34,8 +39,11 @@ object Getter {
       media match {
         case "tf1" =>
           ParserTF1.parseTF1Home(s"$urlMedia$pagination") ++
-            ParserTF1.parseTF1Home(s"$tf1WeekendUrlPagination$pagination")
-        case _ => ParserFranceTelevision.parseFranceTelevisionHome(s"$urlMedia$pagination")
+            ParserTF1.parseTF1Home(s"$tf1WeekendUrlPagination$pagination") ++
+            ParserTF1.parseTF1Home(s"$urlMedia13h$pagination")
+        case _ =>
+          ParserFranceTelevision.parseFranceTelevisionHome(s"$urlMedia$pagination") ++
+            ParserFranceTelevision.parseFranceTelevisionHome(s"$urlMedia13h$pagination")
       }
     }
 
