768d92e9cdf62e3959c6d50a16122866a1fb1f1e
Paul Leclercq <paleclercq@gmail.com>
2023-12-23 16:18:33 +0100

    fix: deploy scaleway crashbackloop

--- src/main/scala/com/github/polomarcus/main/SaveTVNewsToPostgres.scala
+++ src/main/scala/com/github/polomarcus/main/SaveTVNewsToPostgres.scala
@@ -3,7 +3,9 @@ package com.github.polomarcus.main
 import com.github.polomarcus.storage.StorageService
 import com.github.polomarcus.utils.SparkService
 import com.typesafe.scalalogging.Logger
-import org.apache.spark.sql.{DataFrame, SaveMode}
+import org.apache.spark.sql.DataFrame
+
+import java.lang.Thread.sleep
 
 object SaveTVNewsToPostgres {
   def main(args: Array[String]) {
@@ -14,6 +16,7 @@ object SaveTVNewsToPostgres {
     val newsDFTmp: DataFrame = StorageService.readNews().toDF()
     val newsDF = StorageService.resetContainsGlobalWarming(newsDFTmp)
 
+    val scaleway = sys.env.getOrElse("SCALEWAY", "false")
     val dbHost = sys.env.getOrElse("POSTGRES_HOST", "postgres")
     val dbPort = sys.env.getOrElse("POSTGRES_PORT", "5432")
     val dbName = sys.env.getOrElse("POSTGRES_DB", "metabase")
@@ -23,17 +26,24 @@ object SaveTVNewsToPostgres {
     logger.warn(s"Connecting to $connectionUrl")
     // Overwrite new data to a JDBC source
     logger.info(s"Overwriting all previous data with new ones")
-    newsDF.write
-      .format("jdbc")
-      .option("url", connectionUrl)
-      .option("driver", "org.postgresql.Driver")
-      .option("dbtable", dbTable)
-      .option("user", dbUser)
-      .option("password", dbPassword)
-      .mode(SaveMode.Overwrite)
-      .save()
+//    newsDF.write
+//      .format("jdbc")
+//      .option("url", connectionUrl)
+//      .option("driver", "org.postgresql.Driver")
+//      .option("dbtable", dbTable)
+//      .option("user", dbUser)
+//      .option("password", dbPassword)
+//      .mode(SaveMode.Overwrite)
+//      .save()
 
-    logger.info("Saved news inside PG database")
+    logger.info(s"Saved news inside PG database in table ${dbTable}")
+
+    // Without this we have a CrashLoopBackOff (Kubernetes health error)
+    if (scaleway != "false") {
+      val minutesToSleep = 15
+      logger.warn(s"Scaleway : Sleeping for ${minutesToSleep} to avoid CrashLoopBackOff")
+      sleep(1000 * 60 * minutesToSleep)
+    }
 
     spark.stop()
     System.exit(0)
