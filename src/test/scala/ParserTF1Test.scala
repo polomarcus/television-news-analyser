021bed22db50143e34ea5c958c7691fbe66a5a4f
Paul Leclercq <paleclercq@gmail.com>
2024-07-03 15:14:38 +0200

    fix: tf1 parser

--- src/test/scala/ParserTF1Test.scala
+++ src/test/scala/ParserTF1Test.scala
@@ -1,11 +1,7 @@
-import com.github.polomarcus.html.Getter.logger
 import com.github.polomarcus.html.ParserTF1
 import com.github.polomarcus.model.News
 import com.github.polomarcus.utils.DateService
 import org.scalatest.funsuite.AnyFunSuite
-
-import java.sql.Timestamp
-import java.util.Date
 
 class ParserTF1Test extends AnyFunSuite {
   val localhost = "http://localhost:8000"
@@ -13,14 +9,14 @@ class ParserTF1Test extends AnyFunSuite {
     val listNews = ParserTF1.parseTF1HomeHelper(s"$localhost/home-tv-news-tf1.html", localhost)
 
     val description =
-      "L'essentiel Certes, ils sont en général moins chers, mais ils ont la réputation d’être de moins bonne qualité que les articles de grandes marques. Un produit sur trois, vendus en France, est une marque distributeur. Que valent-ils vraiment ?  ans les rayons, elles sont partout. U, Carrefour, Auchan, Franprix, ce sont les marques des distributeurs. Leur vente ne cesse d’augmenter. Ces produits coûtent en moyenne 30% de moins que les marques nationales. Des petits prix qui séduisent les consommateurs. Produits d’hygiène, pâtes, surgelés, pour fidéliser leurs clients, les distributeurs créent sans cesse de nouveaux produits.  Un produit sur trois vendus en France est une marque de distributeur. Que valent-ils vraiment ? Sont-ils moins bons ou meilleurs que les grandes marques ? Première surprise, ils sont souvent fabriqués dans les mêmes usines. Comme ici, des salades sont mises en sachet pour une marque de distributeur. À droite, pour une marque nationale.  Mais il y a quand même quelques différences. En clair, les grandes marques privilégient les parties les plus nobles. C’est ce qui explique l'écart de prix. Mais ce n’est pas la seule raison. Avec les marques nationales, vous payez bien plus que le produit. Et en regardant de près les étiquettes, on apprend que les qualités nutritionnelles sont souvent identiques.  T F1 | Reportage L. Deschateaux, M. Derre, V. Daran"
+      "En 2023, vous avez peut-être fait partie des 27 millions de Français à avoir tenté votre chance à un jeu de hasard. Comment ces tickets sont-ils fabriqués ? Qui décide que telle ou telle carte imprimée sera gagnante ?"
     val news = News(
       "JT20H - Jardinier, cadre, formateur … dans cette entreprise, les salariés sont tous handicapés",
       description,
       DateService.getTimestampTF1("Publié le 16 novembre 2016 à 20h38"),
       0,
       "",
-      List("L. Deschateaux", "M. Derre", "V. Daran"),
+      Nil,
       "",
       List(""),
       "http://localhost:8000/one-subject-tv-news-tf1.html",
@@ -48,9 +44,9 @@ class ParserTF1Test extends AnyFunSuite {
       ParserTF1.parseDescriptionAuthors("/one-subject-tv-news-tf1.html", localhost)
 
     assert(
-      "L'essentiel Certes, ils sont en général moins chers, mais ils ont la réputation d’être de " == description
+      "En 2023, vous avez peut-être fait partie des 27 millions de Français à avoir tenté votre c" == description
         .take(90))
-    assert(List("L. Deschateaux", "M. Derre", "V. Daran") == authors)
+    assert(Nil == authors)
     assert("" == editor)
     assert(List("") == editorDeputy)
   }
